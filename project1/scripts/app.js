00b310c25f5fb43d10103e694ab9344cd3026b8a
Armando Robledo <armando.robledo@gmail.com>
2016-03-10 09:58:53 -0600

    Background and some styling.

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -3,9 +3,11 @@ function getRandom(){
   return Math.round(Math.random() * 10);
 };
 
+
 var rounds  = 1,
     x       = getRandom(),
-    y       = getRandom();
+    y       = getRandom(),
+    correct = 0;
 
 // 1. Get x and y on screen as a math problem
 $('.problem').text(x + ' + ' + y + ' = ');
@@ -14,15 +16,21 @@ $('input[type="submit"]').click(function(evt) {
   evt.preventDefault();
   var answer = +$('input[name="answer"]').val();
   if (answer === (x + y)){
-    alert('You are correct. Great job!')
     // Add pieces to the rocket
+    correct++
+    $('.scoreboard').text('You have answered ' + correct + ' correctly!')
   } else {
-    alert('Nice try, but that answer is not correct. The answer is ' + (x + y))
+    $('.alerts').text('Nice try, but that answer is not correct. The answer is ' + (x + y))
   } //Resets the value of the answer after a submission has been made.
   $('input[name="answer"]').val('')
 
-// Nothing else runs if all 5 rounds were played
-  if (rounds >= 5) return false;
+// Game stops at 5 correct answers
+  if (correct >= 5) {
+    //call ending function
+    $('.alerts').text('Ready for launch!')
+    $('.launch').toggle();
+    return false;
+  }
 
   x = getRandom();
   y = getRandom();
